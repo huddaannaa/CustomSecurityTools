2d9ea0e9edf29d397fd872e320396320cb1807d5
Hud Seidu Daannaa <huddryl@users.noreply.github.com>
2017-11-12 12:03:36 +0000

    Update IP_address_generator.py

--- IP_address_generator.py
+++ IP_address_generator.py
@@ -1,4 +1,11 @@
 def private_ip_gen(local = False, a_ = [10, 172, 192], end_ = 5):
+    
+    #FUNCTION BY HUD SEIDU DAANNAA |MSc |CEH
+    #this is a function i wrote when i was board, but can be very useful, when a range of IP's are need
+    #when for IPs in private_ip_gen():, which can be located below is used in conjuction with this function
+    #with the required operation in the the loop, where for every itteration an IP is allocated into the operational 
+    #function from the IP_gen, which will act as a bigger list
+    
     ip_list = []
     if local == True:
         local   = '127.0.0.1'
@@ -35,3 +42,5 @@ def private_ip_gen(local = False, a_ = [10, 172, 192], end_ = 5):
                         #print ip
                         ip_list.append(ip)
     return ip_list
+for IPs in private_ip_gen():
+    print IPs
