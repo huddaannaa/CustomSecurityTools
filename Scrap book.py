8369895b21b745600d13f7b50321d12edee0ce41
Hud Seidu Daannaa <huddryl@users.noreply.github.com>
2017-11-12 19:47:31 +0000

    Update Scrap book.py

--- Scrap book.py
+++ Scrap book.py
@@ -1,41 +1,57 @@
-import scapy
-target = ""
-pkt_syn_ack = IP(dst=target)/TCP(dport=80, flags="SA")
-pkt_icmp    = IP(dst=target)/ICMP(type=3,code=1)
-pkt_fin     = IP(dst=target)/TCP(dport=22,sport=RandShort(),seq=RandShort(),flags="F")
+def send_a_packet(target, option = 1):
+    """
+    SCRIPT BY HUD SEIDU DAANNAA MSc |CEH
+    This script can be modified and different types 
+    of packets can be crafted and the function can be 
+    used in a "for loop" with a specified itteration
+    to send packets to a given target.
+    
+    the scenario could change were difference types 
+    of packets could be sent randomly.
+    
+    target    = ''      #IP address
+    options 1 = syn_ack
+    ________2 = icmp
+    ________3 = fin
+    
+    """
+    import scapy
+    #syn_ack packet
+    pkt_syn_ack = IP(dst=target)/TCP(dport=80, flags="SA")
 
-#SENDING PACKETS AT THE NETWORK LAYER
-option = #1,2,3
-if option is 1:
-	type_pkt = pkt_syn_ack
-	type_pkt.summary()
-	#type_pkt.show()
-	res,res1 = sr(type_pkt)
-	print res,res1
-else:
-	pass
-if option is 2:
-	type_pkt = pkt_icmp
-	type_pkt.summary()
-	#type_pkt.show()
-	res,res1 = sr(type_pkt)
-	print res,res1
-else:
-	pass
-if option is 3:
-	type_pkt = pkt_fin
-	type_pkt.summary()
-	#type_pkt.show()
-	res,res1 = sr(type_pkt)
-	print res,res1
-else:
-	pass
+    #ICMP packet
+    pkt_icmp    = IP(dst=target)/ICMP(type=3,code=1)
 
-#SENDING PACKETS AT THE ETHERNET LAYER
-packet = 
-res, res2 = srp(packet)
+    #Fin packet
+    pkt_fin     = IP(dst=target)/TCP(dport=22,sport=RandShort(),seq=RandShort(),flags="F")
 
-#HOST DISCOVERY
-                        #MAC Address                  #IP Address
-r1, r2 = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(dst2="192.168.122.0/24"),timeout=4)
-r1.summary(lambda(s,r): r.sprintf("Ether:%Ether.src% \t\t Host: %ARP.psrc%"))
+    #SENDING PACKETS AT THE NETWORK LAYER
+    
+    if option is 1:
+        type_pkt = pkt_syn_ack
+        type_pkt.summary()
+        #type_pkt.show()
+        res,res1 = sr(type_pkt)
+        print res,res1
+    else:
+        pass
+    "_____"
+    if option is 2:
+        type_pkt = pkt_icmp
+        type_pkt.summary()
+        #type_pkt.show()
+        res,res1 = sr(type_pkt)
+        print res,res1
+    else:
+        pass
+
+    "_____"
+    if option is 3:
+        type_pkt = pkt_fin
+        type_pkt.summary()
+        #type_pkt.show()
+        res,res1 = sr(type_pkt)
+        print res,res1
+    else:
+        pass
+    
