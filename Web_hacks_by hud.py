c0b852f7e3fb026e16eaeff19680154290f5a5c1
Hud Seidu Daannaa <huddryl@users.noreply.github.com>
2017-11-12 11:44:02 +0000

    Update Web_hacks_by hud.py

--- Web_hacks_by hud.py
+++ Web_hacks_by hud.py
@@ -1,12 +1,21 @@
-def banner_grab(parsed_args):
+def banner_grab(host, port = 80):
+    #Script by Hud Seidu Daannaa
+    #this function takes 2 parameters, the (host IP and the Port)
+    #default port is 80
+    #_______________________________
+    ''' Banner grabbing can be used to find network hosts that are 
+        running versions of applications and operating systems with known
+        exploits or glean information about a computer system on a network 
+        and the services running on its open ports.'''
     import socket, re
     try:
+        #establishing connection
         sk = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        sk.connect((parsed_args, 80))
+        sk.connect((host, port))
     except:
         print 'connection error'
 
-    h_get = b"GET / HTTP/1.1\nHost: "+parsed_args+"\n\n"
+    h_get = b"GET / HTTP/1.1\nHost: "+host+"\n\n"
     data = ''
     try:
         sk.sendall(h_get)
@@ -25,6 +34,8 @@ def banner_grab(parsed_args):
                 print(s)
             else:
                 pass
+          
+            
 def headers(link):
     import requests
     r = requests.get(link,timeout = 5)
@@ -32,11 +43,21 @@ def headers(link):
     res  = [(header,value) for header, value in r.headers.items()]
     req  = [(header1,value1) for header1, value1 in r.request.headers.items()]
     return stat, res, req
+
+
 def Extract_Urls_4rm_syte(url):
     from bs4 import BeautifulSoup
     import requests
     soup = BeautifulSoup(requests.get('http://' + url).text, 'lxml')
     return [link.get('href') for link in soup.find_all('a')]
+
+
+def email_extractor(string):
+    import re
+    sp = re.split(r'\s|,', string)
+    return [re.match(r'\w*@\w*.\w*.\w*', sp[n]).group() for n in range(len(sp))]
+
+
 def img_pdf_scraper(url):
     from lxml import html
     import requests
